edbf7f6a6cab9d19ba62026c12e5cf6fb00790f5
dev-vintrack <developer@vintrack.com.mx>
2026-07-07 13:43:44 -0600

    fix: import correcto del modelo User en CreditPurchaseController

--- app/Presentation/Http/Controllers/Web/Admin/CreditPurchaseController.php
+++ app/Presentation/Http/Controllers/Web/Admin/CreditPurchaseController.php
@@ -5,7 +5,7 @@ namespace App\Presentation\Http\Controllers\Web\Admin;
 use App\Application\Credits\CommandHandlers\AddCreditsCommandHandler;
 use App\Application\Credits\Commands\AddCreditsCommand;
 use App\Infrastructure\Persistence\Models\Provider;
-use App\Infrastructure\Persistence\Models\User;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
